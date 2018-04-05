880d46499c98d66fa09f211e905ab5c71c5dd9b9
Ross Curley <19098081@student.curtin.edu.au>
2018-04-05 10:48:54 +0800

    left shift

--- assignment/DES/KeyGen.cpp
+++ assignment/DES/KeyGen.cpp
@@ -2,6 +2,7 @@
 
 KeyGen::KeyGen(bitset<10> key)
 {
+    cout << key << endl;
     generate(key);
 }
 
@@ -26,23 +27,27 @@ void KeyGen::generate(bitset<10> key)
 
 bitset<10> KeyGen::permutate10(bitset<10> key)
 {
-    bitset<10> ip = NULL;
+    cout << key << endl;
+    bitset<10> newKey;
     for (int i = 0; i < 10; i++)
     {
-        ip[i] = key[P10[i]];
+        newKey[i] = key[P10[i]];
     }
 
-    return ip;
+    cout << newKey << endl;
+    return newKey;
 }
 
 bitset<8> KeyGen::permutate8(bitset<10> key)
-{
-    bitset<8> newKey = NULL;
+{   
+    cout << key <<endl;
+    bitset<8> newKey;
     for (int i = 0; i < 8; i++)
     {
         key[i] = newKey[P8[i]];
     }
 
+    cout << newKey << endl;
     return newKey;
 }
 
@@ -50,6 +55,37 @@ bitset<10> KeyGen::leftShift(bitset<10> key, int shift)
 {
     bitset<5> left;
     bitset<5> right;
+
+    for (int i = 0; i < 5; i ++)
+    {
+        left[i] = key[i];
+    }
+
+    cout << left << endl;
+
+    for (int i = 5; i < 10; i ++)
+    {
+        right[i] = key[i];
+    }
+
+    cout << right << endl;
+
+    left << shift;
+    right << shift;
+
+    for (int i = 0; i < 5; i ++)
+    {
+        key[i] = left[i];
+    }
+
+    for (int i = 5; i < 10; i ++)
+    {
+        key[i] = right[i];
+    }
+
+    cout << key << endl;
+
+    return key;
 }
 
 
--- assignment/DES/Main.cpp
+++ assignment/DES/Main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <bitset>
+#include "KeyGen.h"
 
 int main()
 {
-    
+    KeyGen kg(1010110011);
+
+    std::cout << kg.getKeyOne() << std::endl;
+    std::cout << kg.getKeyTwo() << std::endl;
+
+    return 0;
 }
