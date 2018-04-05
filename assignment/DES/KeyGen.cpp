758342d09359148981fdb1719f08bfbf2c9feca5
Ross Curley <19098081@student.curtin.edu.au>
2018-04-05 22:21:36 +0800

    fix left shift and p8

--- assignment/DES/KeyGen.cpp
+++ assignment/DES/KeyGen.cpp
@@ -2,7 +2,6 @@
 
 KeyGen::KeyGen(bitset<10> key)
 {
-    cout << key << endl;
     generate(key);
 }
 
@@ -27,27 +26,23 @@ void KeyGen::generate(bitset<10> key)
 
 bitset<10> KeyGen::permutate10(bitset<10> key)
 {
-    cout << key << endl;
     bitset<10> newKey;
     for (int i = 0; i < 10; i++)
     {
         newKey[i] = key[P10[i]];
     }
 
-    cout << newKey << endl;
     return newKey;
 }
 
 bitset<8> KeyGen::permutate8(bitset<10> key)
 {   
-    cout << key <<endl;
     bitset<8> newKey;
     for (int i = 0; i < 8; i++)
     {
-        key[i] = newKey[P8[i]];
+        newKey[P8[i]] = key[i];
     }
 
-    cout << newKey << endl;
     return newKey;
 }
 
@@ -61,17 +56,16 @@ bitset<10> KeyGen::leftShift(bitset<10> key, int shift)
         left[i] = key[i];
     }
 
-    cout << left << endl;
 
     for (int i = 5; i < 10; i ++)
     {
-        right[i] = key[i];
+        right[i-5] = key[i];
     }
 
-    cout << right << endl;
-
-    left << shift;
-    right << shift;
+    cout << "Before bit shift\n" << left << "\n" << right <<endl;
+    left <<= shift;
+    right <<= shift;
+    cout << "after bit shift\n" << left << "\n" << right <<endl;
 
     for (int i = 0; i < 5; i ++)
     {
@@ -80,10 +74,8 @@ bitset<10> KeyGen::leftShift(bitset<10> key, int shift)
 
     for (int i = 5; i < 10; i ++)
     {
-        key[i] = right[i];
+        key[i] = right[i-5];
     }
-
-    cout << key << endl;
 
     return key;
 }
